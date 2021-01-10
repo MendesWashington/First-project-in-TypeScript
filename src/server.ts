a817a36343278e2190fd095f9b644e6b3a24336c
mendesswashington <mendesswashington@gmail.com>
2021-01-10 02:33:01 -0300

    Create routes and config debug used vscode

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from '../routes';
 
 const app = express();
-
-app.get('/', (request, response)=>{
-    return response.status(200).json({message: 'Hello Word!'});
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, ()=>{
     console.log('Server started!')
